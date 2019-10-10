f1f7d993e73e5a88f97fa70284be7c114ad325a1
nitneq <quentinbeugnot@gmail.com>
2019-10-09 22:16:23 -0700

    Tab clicks, grid size and centered

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ const track1_seq = document.getElementById('track1_seq');
 
 function ShowInstr_1(){
   seq.style.display = "none";
+  track1_instr.classList.add("selected-tab");
+  track1_seq.classList.remove("selected");
 }
 
 function ShowSeq_1(){
   seq.style.display = "grid";
+  track1_seq.classList.add("selected");
+  track1_instr.classList.remove("selected-tab");
 }
 
 track1_instr.addEventListener('click',ShowInstr_1);
